05b2951d9b61bdd373a14b4dab7c4ac5839ffb2e
Natalia <natalia.jasiczak96@gmail.com>
2023-10-01 17:28:50 +0200

    Echo GET Data

--- DRF/backend/cfehome/api/views.py
+++ DRF/backend/cfehome/api/views.py
@@ -1,5 +1,24 @@
 from django.http import JsonResponse
+import json
 
+# Django Models
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # requests -> HttpRequest -> Django
+    # print(dir(request))
+    # requests.body
+    print(request.GET) # url query params
+    print(request.POST)
+    body = request.body  # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)  # string og JSON data -> Python Dict
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.headers  # request.META ->
+    data['params'] = dict(request.GET)
+    print(request.headers)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
